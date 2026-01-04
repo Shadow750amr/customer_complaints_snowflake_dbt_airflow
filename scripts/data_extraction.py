45258b64b5a28c3599d4cd71270cef8ba61c6db3
MARCO ANTONIO RAMIREZ GODINEZ <shadow750@Mac-mini-de-MARCO.local>
2026-01-03 19:01:49 -0600

    Some changes in the extraction layer

--- scripts/data_extraction.py
+++ scripts/data_extraction.py
@@ -1,15 +1,22 @@
 import requests
 import logging
 
-# Configuración de logging para ver qué pasa
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+ 
+logger = logging.getLogger(__name__) #tracking
 
 URL = 'https://repodatos.atdt.gob.mx/api_update/profeco/quejas_buro_comercial/buro_comercial_2019_2025.csv'
 CSV_NAME = 'complaints_data.csv'
 
+
 class Extraction:
-    def __init__(self, url: str, filename: str) -> None:
+    '''
+    This class manages archives downloads from an URL and its storage in a local repo.
+    Atributes: url (str): resource path
+               filename (str): the file's name (also cona be a desination path)
+    Methods:   Executes customed download process (streamning) and save it.
+
+    '''
+    def __init__(self, url: str, filename: str) -> None:                    # Despite this is declarative I specically used an expected return
         self.url = url
         self.filename = filename
         logger.info("Clase Extraction instanciada correctamente.")
@@ -20,7 +27,7 @@ class Extraction:
             with requests.get(self.url, stream=True) as response:
                 response.raise_for_status()
                 with open(self.filename, 'wb') as file:
-                    for chunk in response.iter_content(chunk_size=8192):
+                    for chunk in response.iter_content(chunk_size=8192):    # Notice the chunk size it is intentional so there is no RAM affectation
                         if chunk:
                             file.write(chunk)
             logger.info(f"Archivo {self.filename} generado con éxito.")
@@ -29,6 +36,7 @@ class Extraction:
             logger.error(f"Error en la descarga: {e}")
 
 if __name__ == "__main__":
-    # Aquí es donde se pasaban los argumentos que daban error
     extractor = Extraction(URL, CSV_NAME)
     extractor.connect_and_save()
+
+    
