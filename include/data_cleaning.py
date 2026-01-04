15a6c450b084988a846181c6920440a3d349b2d7
MARCO ANTONIO RAMIREZ GODINEZ <shadow750@Mac-mini-de-MARCO.local>
2026-01-03 20:36:49 -0600

    Added some try exceptions

--- include/data_cleaning.py
+++ include/data_cleaning.py
@@ -14,27 +14,37 @@ TEXT_COLUMNS = [
 
 DATE_COLUMNS = ["fecha_ingreso"]
 
+logger = logging.getLogger(__name__)
+
 class DataCleaning:
+    
 
     def __init__(self,path:str,text_cols:list,date_cols:list) -> None:
         self.path = path
         self.text_cols = text_cols
         self.date_cols = date_cols
         self.df = None
-
+    
     def clean_data(self) -> pd.DataFrame:
 
-        # Read the csv
+        logger.info(f"Initializing cleaning for file {self.path}")
+
+    # Read the csv
         self.df = pd.read_csv(self.path)
-        # string columns
-        for col in self.text_cols:
-            if col in self.df.columns:
-                self.df[col] = self.df[col].astype(str).str.strip()
-        #  date columns
-        for col in self.date_cols:
-            if col in self.df.columns:
-                self.df[col] = pd.to_datetime(self.df[col],errors='coerce')
-        return self.df
+        try:
+    # string columns
+            for col in self.text_cols:
+                if col in self.df.columns:
+                    self.df[col] = self.df[col].astype(str).str.strip()
+    #  date columns
+            for col in self.date_cols:
+                if col in self.df.columns:
+                    self.df[col] = pd.to_datetime(self.df[col],errors='coerce')
+                    return self.df
+        except Exception as e:
+            logger.error(f"Could not clean data {e}")
+    
+
 
 if __name__ == "__main__":
     data = DataCleaning(PATH,TEXT_COLUMNS,DATE_COLUMNS)
