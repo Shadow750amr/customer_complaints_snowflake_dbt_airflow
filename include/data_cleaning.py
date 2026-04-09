3c8de1a5518e0d07be13407203bbca7166da6e87
Shadow750amr <shadow750@LAPTOP-AI1GAED1.localdomain>
2026-04-08 18:24:23 -0600

    se agregan notas a data cleaning

--- include/data_cleaning.py
+++ include/data_cleaning.py
@@ -23,6 +23,23 @@ DATE_COLUMNS = DATE_COLUMNS
 
 class DataCleaning:
 
+    ''' 
+    The class is designed to handle the cleaning process of your data, particularly focusing on text and date columns.
+
+    - Constructor (__init__):
+
+    Takes the file path, list of text columns and a list of date columns as input.
+    Stores these details in instance attributes: self.path, self.text_cols, self.date_cols and self.df.
+    
+    - Cleaning (clean_data): This method performs the cleaning operations:
+    Reads the CSV file into a pandas DataFrame (self.df).
+    Handles text column cleanup: Uses .astype(str).str.strip() for removing leading/trailing whitespace from each column of the text type.
+    Handles date column conversion and returns the cleaned DataFrame.
+    
+      Usage: The if __name__ == "__main__": block demonstrates how to create an instance of the DataCleaning class and call the 
+    clean_data method.
+    '''
+
     def __init__(self,path:str,text_cols:list,date_cols:list) -> None:
         self.path = path
         self.text_cols = text_cols
