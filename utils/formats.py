cbe3a715d1c80b6d342d84cb28d6ace4f413ae0b
Shadow750amr <shadow750@LAPTOP-AI1GAED1.localdomain>
2026-04-08 18:23:31 -0600

    se agregan utils

---
+++ utils/formats.py
@@ -0,0 +1,9 @@
+# Defining the columns for further transformations
+TEXT_COLUMNS = [
+    "expediente", "estado_procesal", "razon_social", 
+    "nombre_comercial", "giro", "sector", 
+    "area_responsable", "estado", "motivo_reclamacion"
+]
+
+DATE_COLUMNS = ["fecha_ingreso"]
+
